9d0bac675ac752142e3cc8a4aeeb97d0805d6007
fogelkuba <fogelkuba@gmail.com>
2018-07-31 22:24:11 +0200

    fix

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@angular/core';
 
+import {Observable} from 'rxjs/index';
+import {AngularFireAuth} from 'angularfire2/auth';
+
 @Injectable()
 export class AuthService {
-
-  constructor() { }
+  private user: Observable<firebase.user>;
+  constructor(
+    private firebaseAuth: AngularFireAuth
+  ) {
+    this.user = firebaseAuth.authState;
+  }
 
 }
