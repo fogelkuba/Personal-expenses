849038d094ec2482ea4e3e0d508d26220d58b236
fogelkuba <fogelkuba@gmail.com>
2018-07-31 22:35:08 +0200

    fix

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 
 import {Observable} from 'rxjs/index';
 import {AngularFireAuth} from 'angularfire2/auth';
@@ -6,10 +6,34 @@ import {AngularFireAuth} from 'angularfire2/auth';
 @Injectable()
 export class AuthService {
   private user: Observable<firebase.user>;
+
   constructor(
     private firebaseAuth: AngularFireAuth
   ) {
     this.user = firebaseAuth.authState;
   }
+  login() {
 
+  }
+
+  signInWithGoogle() {
+    return this.firebaseAuth.auth.signInWithPopup(
+      new firebase.auth.GoogleAuthProvider()
+    );
+  }
+
+  isLoggedIn() {
+    if (this.userDetails == null ) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  logout() {
+    this.firebaseAuth.auth.signOut()
+      .then((res) => {
+        console.log(res);
+      });
+  }
 }
